70a3eb0644594b60952f1c7ad122e5373902ea09
SeungJin051 <seungjin051@gmail.com>
2024-09-26 10:33:48 +0900

    Feat: 데이터 가져오기
    요청 워터폴 발생 -> 병렬 데이터 페칭 -> Promise.all() 또는 Promise.allSettled()

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,47 @@
+import {
+  fetchCardData,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
+import { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
 
 export default async function Page() {
+  // 요청 워터폴 발생
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices(); // fetchRevenue가 끝나고 실행
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  }: {
+    totalPaidInvoices: string;
+    totalPendingInvoices: string;
+    numberOfInvoices: number;
+    numberOfCustomers: number;
+  } = await fetchCardData(); // fetchLatestInvoices가 끝나고 실행
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
